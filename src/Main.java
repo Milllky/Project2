2e95be5d82bdc332eff6c9ed8d6ece5ef02e3f5d
Trofimov M <milllky@rocketmail.com>
2025-01-23 22:49:43 +0500

    Reformated code

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
-
     public static void main(String[] args) {
+
         int price = 24678;
         int bonus = 20;
-
         int amount = price / bonus;
 
-    System.out.println("Бонусные мили: " + amount);
+        System.out.println("Бонусные мили: " + amount);
     }
 }
