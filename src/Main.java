d55b4318b80846c921b200963d7e94e879fdad3f
Trofimov M <milllky@rocketmail.com>
2025-01-26 15:25:26 +0500

    Add BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int price = 24678;
-        int bonus = 20;
-        int amount = price / bonus;
-
-        System.out.println("Бонусные мили: " + amount);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println("Бонусные мили: " + miles);
     }
 }
